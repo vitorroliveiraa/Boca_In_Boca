bc4f6d6212e94ea86b7c1da9bef405e2bf1ac91b
Vitor Oliveira <vitorroliveiraa@outlook.com.br>
2021-07-27 19:25:34 -0300

    Rota post com erro

---
+++ src/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const { v4: uuid } = require("uuid");
+const port = 3333;
+
+const users = [];
+
+app.get("/users", (request, response) => {
+    return response.json(users);
+});
+
+app.post("/users", (request, response) => {
+    const {
+        username,
+        name,
+        email,
+        phone,
+    } = request.body;
+
+    const userAlreadyExist = users.find(
+        (user) => user.username === username
+    );
+
+    if (userAlreadyExist) {
+        return response.status(400).json(
+            { error: "Username already exist!" }
+        );
+    }
+
+    const newUser = {
+        id: uuidv4(),
+        created_at: new Date(),
+        username,
+        score: 0,
+        level: 0,
+        name,
+        email,
+        phone,
+        address: []
+    }
+
+    users.push(newUser);
+
+    return response.status(201).json(
+        { success: "User created successfully!" }
+    );
+});
+
+app.listen(port, () => {
+    console.log(`Started application at http://localhost:${port} ⚡`);
+});
